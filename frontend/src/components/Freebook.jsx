806f9d1b8f5d525a33353d4987b026f512b356da
Vaidehi Umbarkar <149375743+umbarkarvaidehi18@users.noreply.github.com>
2024-10-22 13:29:32 +0530

    Update Freebook.jsx

--- frontend/src/components/Freebook.jsx
+++ frontend/src/components/Freebook.jsx
@@ -12,7 +12,7 @@ function Freebook() {
   useEffect(() => {
     const getBook = async () => {
       try {
-        const res = await axios.get("https://bookstore-app-backend-x8xl.onrender.com/book");
+        const res = await axios.get("https://bookstore-app-backend-x8xl.onrender.com");
         const result = res.data.filter((data) => data.category === "Free");
         setBook(result);
         console.log(result);
